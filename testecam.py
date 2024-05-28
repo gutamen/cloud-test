c531284f1a3b25d1036db6755b19835c53fb2d4e
Gustavo A Martini <gutamen@live.com>
2024-05-28 10:24:41 -0300

    teste

--- testecam.py
+++ testecam.py
@@ -1,12 +1,34 @@
 import cv2
 
 # URI do stream RTSP da câmera
-uri_stream = 'rtsp://admin:Gustavo2606@24.152.102.197:55555/videoMain'
-
+uri_stream = 'rtsp://admin:Gustavo2606@24.152.102.197:25565/videoMain'
+#uri_stream = 'rtsp://admin:Gustavo2606@192.168.1.101:25565/videoMain'
 
 # Abrindo o stream de vídeo com OpenCV
 cap = cv2.VideoCapture(uri_stream)
 
+"""
+if not cap.isOpened():
+    print("Erro ao abrir o stream de vídeo.")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Falha ao obter frame do stream.")
+        break
+
+    # Exibindo o frame de vídeo
+    cv2.imshow('Video Stream', frame)
+
+    # Pressione 'q' para sair da exibição
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+"""
 if not cap.isOpened():
     print("Erro ao abrir o stream de vídeo.")
     exit()
@@ -17,7 +39,7 @@ if not ret:
     print("Falha ao obter frame do stream.")
 else:
     cv2.imshow('Video Stream', frame)
-
+    cv2.imwrite('captured_image.jpg', frame)
     # Pressione 'q' para sair da exibição
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
